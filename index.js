cdd13316f467e29341859fb8e79d4a9888f2b011
Mingdong Luo <mdluo@nsmss.com>
2016-06-18 23:31:14 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * Better Timeago English Locale
+ * (C) 2016 Mingdong Luo (https://github.com/mdluo) | MIT License
+ */
+
+module.exports = {
+  fecha: {
+    dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+    dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    amPm: ['am', 'pm'],
+    // D is the day of the month, function returns something like...  3rd or 11th
+    DoFn: function (D) {
+        return D + [ 'th', 'st', 'nd', 'rd' ][ D % 10 > 3 ? 0 : (D - D % 10 !== 10) * D % 10 ];
+    }
+  },
+  units: {
+    moment:   ["Just now"],
+    second:   [" second", "s"],
+    minute:   [" minute", "m"],
+    hour:     [" hour", "h"],
+    day:      [" day", "d"],
+    week:     [" week", " week"],
+    month:    [" month", " month"],
+    year:     [" year", " year"]
+  },
+  types: {
+    relative: {
+      ago:     " ago",
+      in:      "In",
+    },
+    absolute: [
+      {k: 'now', v: [{f: true, s: "YYYY/M/D HH:mm"}]},
+      {k: 'today', v: [{s: "Today"}, {f: true, s:" HH:mm"}]},
+      {k: 'yesterday', v: [{s: "Yesterday"}, {f: true, s: " HH:mm"}]},
+      //{k: 'thisWeek', v: [{f: true, s: "dddd"}]},
+      //{k: 'lastWeek', v: [{s: "Last"}, {f: true, s:" ddd"}]},
+      {k: 'thisYear', v: [{f: true, s: "MMM Do"}]},
+      {k: 'epoch', v: [{f: true, s: "YYYY/M/D"}]}
+    ],
+  },
+  startOfWeek:  0,
+  errors: {
+    dafault:    "Unknow time"
+  }
+}
